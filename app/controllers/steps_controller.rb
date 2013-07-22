a506febcd70108502dc2607e155d7131252897b3
bethanyr <bethany.rentz@hotmail.com>
2013-07-21 21:42:29 -0700

    can now create steps

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,5 +1,6 @@
 class StepsController < ApplicationController
 	before_filter :find_instructionguide
+	before_filter :find_step, :only => [:show, :edit, :update, :destroy]
 	def new
 		@step = @instructionguide.steps.build
 	end
@@ -12,8 +13,22 @@ class StepsController < ApplicationController
 
 	end
 
+	def create
+		@step = @instructionguide.steps.build(params[:step])
+		if @step.save
+			flash[:notice] = "Step has been created."
+			redirect_to [@instructionguide, @step]
+		else
+			flash[:alert] = "Step has not been created."
+			render :action => "new"
+		end
+	end
+
 	private
 	def find_instructionguide
 		@instructionguide = Instructionguide.find(params[:instructionguide_id])
+	end
+	def find_step
+	  	@step = @instructionguide.steps.find(params[:id])
 	end
 end
--- spec/integration/creating_steps_spec.rb
+++ spec/integration/creating_steps_spec.rb
@@ -2,4 +2,19 @@
 
 # Creating steps without required attributes fails
 
+require 'spec_helper'
+
+feature 'Creating Steps for an Instruction Guide' do
+	scenario "can create steps for an instruction guide" do
+		instructionguide= FactoryGirl.create(:instructionguide, :name => "How to Make a sandwich")
+		visit '/'
+		click_link "How to Make a sandwich"
+		click_link "New Step"
+		fill_in "Title", :with => "Bake a loaf of bread"
+		fill_in "Order", :with => 1
+		click_button "Create Step"
+		page.should have_content ("Step has been created.")
+
+	end
+  end	
 
